0512f5a5aab6d76fe98acec91708073c8e147cf3
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2020-10-07 09:39:34 +0200

    Rename framework to XCData
    Swift doesn't like it if the framework has the same name as a struct or classes
    defined in the framework. In our case it was the XCollectionData class which
    produced the compiler error. Therefore the framework is now called XCData.

--- Sources/XCollectionData/XTableSection.swift
+++ Sources/XCollectionData/XTableSection.swift
@@ -11,6 +11,13 @@ open class XTableSection: XCollectionSection {
     public let header: String?
     public let footer: String?
     
+    public override init(identifier: String) {
+        header = nil
+        footer = nil
+        
+        super.init(identifier: identifier)
+    }
+    
     public init(identifier: String, header: String? = nil, footer: String? = nil) {
         self.header = header
         self.footer = footer
